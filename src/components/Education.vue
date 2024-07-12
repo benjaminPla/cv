edb1b22bea59f458c6bc69b0b8913a07fbbed4b3
ben <benjaminpla.dev@gmail.com>
2024-07-12 13:38:43 +0200

    update Education components

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -5,55 +5,61 @@
     </h1>
     <div>
       <h2>{{ $t("message.education.diploma") }}</h2>
-      <div class="diplomas-grid">
-        <img
-          alt="mug_securecoding diploma"
-          class="diploma"
-          src="../assets/mug_securecoding.jpg"
-        />
+      <div class="grid">
+        <img alt="alkemy diploma" class="diploma" src="../assets/alkemy.jpg" />
         <img
           alt="acamica_digitalhouse diploma"
           class="diploma"
           src="../assets/acamica_digitalhouse.jpg"
         />
-        <img alt="alkemy diploma" class="diploma" src="../assets/alkemy.jpg" />
+        <img
+          alt="machine learning diploma"
+          class="diploma"
+          src="../assets/machine_learning.jpg"
+        />
+        <img
+          alt="english b2 diploma"
+          class="diploma"
+          src="../assets/english_b2.jpg"
+        />
+        <img
+          alt="mug_securecoding diploma"
+          class="diploma"
+          src="../assets/mug_securecoding.jpg"
+        />
       </div>
     </div>
-    <div>
+    <div class="margin-top">
       <h2>{{ $t("message.education.languages.title") }}</h2>
-      <div class="card">
-        <p class="card-header">
-          {{ $t("message.education.languages.spanish") }}
-        </p>
-        <p class="card-info">
-          {{ $t("message.education.languages.native") }}
-        </p>
-      </div>
-      <div class="card card-hover" @click="handleDiplomaClick('english')">
-        <p class="card-header">
-          {{ $t("message.education.languages.english") }}
-        </p>
-        <p class="card-info">
-          {{ $t("message.education.languages.intermadiateHigh") }}
-        </p>
-      </div>
-      <div class="card">
-        <p class="card-header">
-          {{ $t("message.education.languages.italian") }}
-        </p>
-        <p class="card-info">
-          {{ $t("message.education.languages.intermadiate") }}
-        </p>
+      <div class="grid">
+        <div class="card">
+          <p class="card-header">
+            {{ $t("message.education.languages.spanish") }}
+          </p>
+          <p class="card-info">
+            {{ $t("message.education.languages.native") }}
+          </p>
+        </div>
+        <div class="card">
+          <p class="card-header">
+            {{ $t("message.education.languages.english") }}
+          </p>
+          <p class="card-info">
+            {{ $t("message.education.languages.intermadiateHigh") }}
+          </p>
+        </div>
+        <div class="card">
+          <p class="card-header">
+            {{ $t("message.education.languages.italian") }}
+          </p>
+          <p class="card-info">
+            {{ $t("message.education.languages.intermadiate") }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-const handleDiplomaClick = (diploma: string): void => {
-  window.open(`../assets/${diploma}`, "_blank");
-};
-</script>
 
 <style scoped lang="scss">
 .content {
@@ -61,7 +67,7 @@ const handleDiplomaClick = (diploma: string): void => {
   display: flex;
   flex-direction: column;
 
-  .diplomas-grid {
+  .grid {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 50px;
@@ -77,6 +83,16 @@ const handleDiplomaClick = (diploma: string): void => {
 
   .diploma {
     border-radius: 20px;
+    width: 100%;
+  }
+
+  .card {
+    min-height: 150px;
+    width: 100%;
+  }
+
+  .margin-top {
+    margin-top: 30px;
     width: 100%;
   }
 }
